602f5393936944c9ea50c5fc0e9fae9a092310d3
Ben Smus <bensmus@gmail.com>
2020-06-10 01:18:42 -0700

    Trying  to hack together a working slideshow

--- slideshow.js
+++ slideshow.js
@@ -4,6 +4,7 @@ const data = [
     {caption: "Jormungandr, image 3/3", src: "jormungandr.jpg"}
 ];
 
+/*
 // load html content depending on slidenumber
 let slidenumber = 0;
 
@@ -12,4 +13,33 @@ let slideshowDescription = document.querySelector('#slideshow-description')
 
 ancientImage.src = data[slidenumber]['src']
 slideshowDescription.innerHTML = data[slidenumber]['caption']
+*/
 
+// global with var, bad form but i'm bad.
+var ancientImage= document.querySelector('#ancient-dragon-image');
+var slideshowDescription = document.querySelector('#slideshow-description')
+
+// index which should always be 0 1 or 2 
+var n = 0;
+
+function displaySlide() {
+    ancientImage.src = data[n]['src']
+    slideshowDescription.innerHTML = data[n]['caption']
+}
+
+function next() {
+    if (n < 2) {
+        n += 1;
+        displaySlide(n);
+    }
+}
+
+function prev() {
+    if (n > 0) {
+        n -= 1;
+        displaySlide(n);
+    }
+}
+
+document.querySelector('#next').onclick = next()
+document.querySelector('#prev').onclick = prev()
